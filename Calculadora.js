c83fcefe2ffe31eef178a8f25ae6628b9967441c
Ramon Rocha <ramonrocha1989@gmail.com>
2020-11-12 16:20:41 -0300

    :sparkles: Cria função multiplicação

--- Calculadora.js
+++ Calculadora.js
@@ -1,3 +1,5 @@
+let  prompt = require('prompt-sync')
+
 function sum(){
     //TODO: Implementar soma
 }
@@ -7,7 +9,15 @@ function sub(){
 }
 
 function mult(){
-    //TODO: Implementar multiplicação
+    let valor1 = Number(prompt("Número 1: "))
+    let valor2 = Number(prompt("Número 2: "))
+
+    if (valor1 == '' || valor2 == '') {
+        console.log('Digite um número!')
+    } else {
+        let soma = valor1 - valor2
+        console.log(`${valor1} - ${valor2} = ${soma}`)
+    }
 }
 
 function div(){
